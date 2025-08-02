6a5e8acd7d13288e2534b3ffcc17bfa999686b49
JaimeQuezadaJr <jaimequezadajr@gmail.com>
2025-08-01 19:00:28 -0700

    first commit

---
+++ PlaneScribe/PlaneScribeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaneScribeApp.swift
+//  PlaneScribe
+//
+//  Created by Jaime Quezada on 8/1/25.
+//
+
+import SwiftUI
+
+@main
+struct PlaneScribeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# PlaneScribe
